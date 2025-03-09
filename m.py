26ea5931ca276f381cefb878f9586f2bdb6601a9
Sneha-Gawas <19gawassneha@gmail.com>
2025-03-09 23:44:38 +0530

    Committed

---
+++ m.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Displaying a local image with a specific width
+
+
+# OR using full container width
+st.image("Bike.png", caption="Bikeride-share Analysis",use_container_width=True)
